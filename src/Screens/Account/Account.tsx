05fdf97a79406f0952fa643eb9e58734731904d6
Tianyi Zheng <tianyizheng02@gmail.com>
2021-08-13 04:03:10 -0700

    Fix account modal formatting

--- src/Screens/Account/Account.tsx
+++ src/Screens/Account/Account.tsx
@@ -1,96 +1,76 @@
 import React from 'react';
 import './Account.css';
 
-interface accountProps {
-  showAccount : boolean;
-  handleShowAccount : () => void;
+interface BankAccountSimple {
+  name: string;
+  lastFour: string;
+  amount: number;
 }
 
-export const Account = ({showAccount, handleShowAccount} : accountProps) => {
-    const accounts: Array<BankAccountSimple> = [
-        {
-            name: "Visa",
-            lastFour: "1234",
-            amount: 1234,
-        },
-        {
-            name: "Capital One",
-            lastFour: "1223",
-            amount: 53442.23,
-        },
-    ];
-    const savings = accounts.map(acc => acc.amount).reduce((acc, cur) => acc + cur);
-    const oldSavings = 12314;
+function BankAccountSimpleCard(props: any) {
+  const account = props.account;
+  return (
+    <div className="row">
+      <div className="column1">
+        <p>{account.name}</p>
+      </div>
+      <div className="column2">
+        <p>{"..." + account.lastFour}</p>
+      </div>
+      <div className="column3">
+        <p>{moneyFormatUSD(account.amount)}</p>
+      </div>
+    </div>
+  )
+}
+
+function moneyFormatUSD(amount: number) {
+  return amount.toLocaleString("en-US", {
+    style: "currency",
+    currency: "USD"
+  })
+}
+
+interface accountProps {
+  showAccount: boolean;
+  handleShowAccount: () => void;
+}
+
+export const Account = ({ showAccount, handleShowAccount }: accountProps) => {
+  const accounts: Array<BankAccountSimple> = [
+    {
+      name: "Visa",
+      lastFour: "1234",
+      amount: 1234,
+    },
+    {
+      name: "Capital One",
+      lastFour: "1223",
+      amount: 53442.23,
+    },
+  ];
+
+  const savings = accounts.map(acc => acc.amount).reduce((acc, cur) => acc + cur);
+  const oldSavings = 12314;
+
   return (
     <div className="section full-height">
-      <input className="modal-btn" style={{paddingLeft: "500px"}} type="checkbox" id="account-btn" name="modal-btn"/>
+      <input className="modal-btn" style={{ paddingLeft: "500px" }} type="checkbox" id="account-btn" name="modal-btn" />
 
-      <label className="text-center" htmlFor="account-btn" style={{marginBottom:"400px", marginLeft:"300px", width:"80px"}}>
-          Account
-          {/*<i className="uil uil-expand-arrows"></i>*/}
-      </label>
+      <label className="text-center" htmlFor="account-btn" style={{ marginBottom: "400px", marginLeft: "300px", width: "80px" }}>Account</label>
 
       <div className="modal">
         <div className="modal-wrap">
           <div className="summary">
-            <h3>
-              <span>
-                  You saved&nbsp;
-              </span>
-              <b>{moneyFormatUSD(savings)}</b>
-              <span>
-                  &nbsp;this month!
-              </span>
-            </h3>
-            <span>
-                That's a change of&nbsp;
-            </span>
-            <b>{moneyFormatUSD(savings - oldSavings)}</b>
-            <span>
-                &nbsp;from last month!
-            </span>
+            <h3> You saved <b>{moneyFormatUSD(savings)}</b> this month!</h3>
+            <p>That's a change of <b>{moneyFormatUSD(savings - oldSavings)}</b> from last month!</p>
             <p>Your savings gained you <i>42 kibble koins</i> for your cats!</p>
           </div>
 
           <h5>View your savings in detail</h5>
-          <div className="bg-opacity-50 m-4 bg-gray-500 rounded-md">
-              {accounts.map(acc =>
-                  (<BankAccountSimpleCard account={acc}/>)
-              )}
+          {accounts.map(acc => (<BankAccountSimpleCard account={acc} />))}
         </div>
       </div>
     </div>
   );
 };
-
-interface BankAccountSimple {
-    name: string;
-    lastFour: string;
-    amount: number;
-}
-
-function BankAccountSimpleCard(props: any) {
-    const account = props.account;
-    return (
-        <div className="flex flex-row flex-auto bg-opacity-75 bg-pink-400 m-2 rounded-md text-indigo-900">
-            <span className="m-2 w-1/2">
-                <div className="text-xl font-bold text-left">
-                    {account.name}
-                </div>
-                <div className="text-sm text-left">
-                    {"..."+account.lastFour}
-                </div>
-            </span>
-            <span className="text-right flex-grow m-1 self-center text-xl">
-                {moneyFormatUSD(account.amount)}
-            </span>
-        </div>
-    )
-}
-
-function moneyFormatUSD(amount: number) {
-    return amount.toLocaleString("en-US", {
-		            style: "currency",
-                    currency: "USD"
-                })
-}

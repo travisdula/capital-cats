a8ced2616d4b8e6dcfb0149ccc4e8c62395abf66
walid101 <nuhankhan101@gmail.com>
2021-08-12 22:39:48 -0400

    Added Shop Options

--- src/Screens/Shop/Shop.tsx
+++ src/Screens/Shop/Shop.tsx
@@ -4,6 +4,9 @@ import './Shop.css';
 import Modal from 'react-bootstrap/Modal';
 import Button from 'react-bootstrap/Button';
 import cat1 from '../../Resources/cat1.png'
+import boot from '../../Resources/boots.png'
+import hat from '../../Resources/hat.png'
+import food from '../../Resources/food.jpg'
 import '../../Utility/Typewriter.css'
 
 interface shopProps {
@@ -14,13 +17,24 @@ export const Shop = ({showShop, handleShowShop} : shopProps) => {
   return (
         
         <div className="section full-height">
-        <input className="modal-btn"  style={{paddingLeft: "500px"}} type="checkbox" id="modal-btn" name="modal-btn" onClick={handleShowShop}/>
+        <input className="modal-btn"  style={{paddingLeft: "500px"}} type="checkbox" id="modal-btn" name="modal-btn"/>
         <label htmlFor="modal-btn" style={{marginBottom:"200px", marginLeft:"300px", width:"80px"}}>Shop<i className="uil uil-expand-arrows"></i></label> 		
         <div className="modal">		
         <div className="modal-wrap">	
         <img src={cat1} alt=""/>	
-          <p className="css-typing">Meow... wish I had some boots.. mew..</p>	          		
-          </div>			          		
+        <div className="row">
+          <div className="column">
+          <img src={hat} style={{width:"100%"}}/>
+          </div>
+          <div className="column">
+            <img src={boot} style={{width:"100%"}}/>
+          </div>
+          <div className="column">
+            <img src={food} style={{width:"100%"}}/>
+          </div>
+        </div>
+        <p className="css-typing">Meow... wish I had some boots.. mew..</p>	          		
+        </div>			          		
         </div>	
 
       <a href="https://front.codes/" className="logo" target="_blank">

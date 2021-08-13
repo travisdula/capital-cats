737ac604865fae2cc3ec63b0fef7b8d68545126e
walid101 <nuhankhan101@gmail.com>
2021-08-12 20:23:50 -0400

    Added Images for homescreen background and shop

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 import {Shop} from './Screens/Shop/Shop'
 import { Navbar } from './Screens/Navbar/Navbar'
+import Background  from './Resources/cat 2.jpg'
 
 function App() {
   const [showShop, setShowShop] = useState(true);
@@ -11,11 +12,10 @@ function App() {
   }
   return (
     <div className="App">
-      {showShop && 
-      <Shop 
+      <img src = {Background} style={{height:"700px", width:"450px"}}/>
+      <Shop
         showShop = {showShop}
         handleShowShop = {handleShowShop}/>
-      }
     </div>
   );
 }
--- src/Screens/Shop/Shop.tsx
+++ src/Screens/Shop/Shop.tsx
@@ -3,6 +3,7 @@ import logo from './logo.svg';
 import './Shop.css';
 import Modal from 'react-bootstrap/Modal';
 import Button from 'react-bootstrap/Button';
+import cat1 from '../../Resources/cat1.png'
 
 interface shopProps {
   showShop : boolean;
@@ -13,10 +14,10 @@ export const Shop = ({showShop, handleShowShop} : shopProps) => {
         
         <div className="section full-height">
         <input className="modal-btn"  style={{paddingLeft: "500px"}} type="checkbox" id="modal-btn" name="modal-btn"/>
-        <label htmlFor="modal-btn" style={{marginTop: "500px", marginLeft:"500px"}}> Open Shop <i className="uil uil-expand-arrows"></i></label> 		
+        <label htmlFor="modal-btn" style={{marginBottom:"200px", marginLeft:"300px", width:"80px"}}>Shop<i className="uil uil-expand-arrows"></i></label> 		
         <div className="modal">		
-          <div className="modal-wrap">	
-        <img src="assets/" alt=""/>	
+        <div className="modal-wrap">	
+        <img src={cat1} alt=""/>	
             <p>A Cat should go in here</p>	          		
           </div>			          		
         </div>	
